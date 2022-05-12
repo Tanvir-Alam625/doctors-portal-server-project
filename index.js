91853c2a4b3b1baf911018fd301bfca37db46d8d
tanveer <mdtanviralamht731@gmail.com>
2022-05-12 13:27:34 +0600

    mongodb connection successfully

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.qgjln.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+client.connect((err) => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  console.log("connection successfully");
+  // client.close();
+});
 app.get("/", (req, res) => {
   res.send("Hello World! doctors portal");
 });
